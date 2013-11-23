fdc96e67470da78c0283793d9a7ace295fce0c59
Santosh Shilimkar <santosh.shilimkar@ti.com>
2013-11-23 18:16:50 -0500

    mm: nobootmem: avoid type warning about alignment value
    Building ARM with NO_BOOTMEM generates below warning. Using min_t
    to find the correct alignment avoids the warning.
    
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Santosh Shilimkar <santosh.shilimkar@ti.com>

--- mm/nobootmem.c
+++ mm/nobootmem.c
@@ -85,7 +85,7 @@ static void __init __free_pages_memory(unsigned long start, unsigned long end)
 	int order;
 
 	while (start < end) {
-		order = min(MAX_ORDER - 1UL, __ffs(start));
+		order = min_t(size_t, MAX_ORDER - 1UL, __ffs(start));
 
 		while (start + (1UL << order) > end)
 			order--;
